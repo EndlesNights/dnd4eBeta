b43224a964362a8776d6eacb59c0d96fdbfd40b1
EndlesNights <endlesnights@gmail.com>
2022-07-28 14:50:33 -0400

    v10 effects

--- module/effects/effects.js
+++ module/effects/effects.js
@@ -44,15 +44,18 @@
 	/** @inheritdoc */
 	async _preCreate(data, options, user) {
 		await super._preCreate(data, options, user);
+
 		// Set initial duration data for Actor-owned effects
 		if ( this.parent instanceof Actor ) {
 			const updates = {duration: {startTime: game.time.worldTime}, transfer: false, equippedRec: false};
 			const combat = game.combat;
-			if ( combat ) {
-
-				updates.flags = {dnd4e: { effectData: { startTurnInit: combat.turns[combat.turn].data.initiative ?? 0}}};
+			if (combat?.turn) {//if combat has started
+				console.log(combat)
+				console.log(combat.turns)
+				console.log(combat.turns[combat.turn])
+				updates.flags = {dnd4e: { effectData: { startTurnInit: combat.turns[combat.turn].initiative ?? 0}}};
 			}
-			this.data.update(updates);
+			this.updateSource(updates);
 		}
 	}
 	/* --------------------------------------------- */
@@ -63,10 +66,10 @@
 	 determineSuppression() {
 		this.isSuppressed = false;
 
-		if ( this.data.disabled || (this.parent.documentName !== "Actor") ) return;
+		if ( this.disabled || (this.parent.documentName !== "Actor") ) return;
 
-		const [parentType, parentId, documentType, documentId] = this.data.origin?.split(".") ?? [];
-		const originArray = this.data.origin?.split(".");
+		const [parentType, parentId, documentType, documentId] = this.origin?.split(".") ?? [];
+		const originArray = this.origin?.split(".");
 
 		// if ( (parentType !== "Actor") || (parentId !== this.parent.id) || (documentType !== "Item") ) return;
 
@@ -82,7 +85,7 @@
 		//types of items that can be equipted
 		const validTypes = ["weapon", "equipment", "consumable", "tool", "loot", "backpack"];
 		if(validTypes.includes(item.type) && item.system.equipped === false){
-			this.isSuppressed = this.data.flags.dnd4e?.effectData?.equippedRec || false;
+			this.isSuppressed = this.flags.dnd4e?.effectData?.equippedRec || false;
 			return;
 		}
 		this.isSuppressed = item.areEffectsSuppressed;
@@ -115,7 +118,7 @@
 			case "delete":
 				return effect.delete();
 			case "toggle":
-				return effect.update({disabled: !effect.data.disabled});
+				return effect.update({disabled: !effect.disabled});
 		}
 	}
 
@@ -132,37 +135,16 @@
 		}
 		
 		return super.isTemporary;
-		// const duration = this.data.duration.seconds ?? (this.data.duration.rounds || this.data.duration.turns) ?? 0;
-		// return (duration > 0) || this.getFlag("core", "statusId");
 	}
 
-	// /* --------------------------------------------- */
-
-	// /**
-	//  * Summarize the active effect duration
-	//  * @type {{type: string, duration: number|null, remaining: number|null, label: string}}
-	//  */
-	// get duration() {
-	// 	const durationType = this.getFlag("dnd4e", "effectData")?.durationType;
-	// 	if(durationType){
-	// 		console.log(durationType)
-	// 		return durationType;
-	// 	}
-
-	// 	return super.duration;
-	// }
-
-	/* --------------------------------------------- */
-
-	
 	/**
 	 * @override
 	 * Summarize the active effect duration
 	 * @type {{type: string, duration: number|null, remaining: number|null, label: string}}
 	 */
 	get duration() {
-		if(this.data.flags?.dnd4e?.effectData?.durationType){
-			const d = this.data.duration;
+		if(this.flags?.dnd4e?.effectData?.durationType){
+			const d = this.duration;
 			const duration = this._getCombatTime(d.rounds, d.turns);
 			return {
 				type: "turns",
@@ -171,8 +153,16 @@
 				label: this._getDurationLabel(d.rounds, d.turns)
 			}
 		}
-		return super.duration;
+		if(super.duration){
+			return super.duration
+		}
+		return {};
 	}
+
+	set duration(duration){
+		return duration;
+	}
+
 	/* -------------------------------------------- */
 
 	/**
@@ -236,7 +226,7 @@
 		for ( let e of effects ) {
 			e._getSourceName(); // Trigger a lookup for the source name
 			if ( e.isSuppressed ) categories.suppressed.effects.push(e);
-			else if ( e.data.disabled ) categories.inactive.effects.push(e);
+			else if ( e.disabled ) categories.inactive.effects.push(e);
 			else if ( e.isTemporary ) categories.temporary.effects.push(e);
 			else categories.passive.effects.push(e);
 		}
@@ -245,68 +235,3 @@
 		return categories;
 	}
 }
-
-
-// /**
-//  * Manage Active Effect instances through the Actor Sheet via effect control buttons.
-//  * @param {MouseEvent} event      The left-click event on the effect control
-//  * @param {Actor|Item} owner      The owning entity which manages this effect
-//  */
-//  export function onManageActiveEffect(event, owner) {
-// 	event.preventDefault();
-// 	const a = event.currentTarget;
-// 	const li = a.closest("li");
-// 	const effect = li.dataset.effectId ? owner.effects.get(li.dataset.effectId) : null;
-// 	switch ( a.dataset.action ) {
-// 		case "create":
-// 			return owner.createEmbeddedDocuments("ActiveEffect", [{
-// 				label: game.i18n.localize("DND4EBETA.EffectNew"),
-// 				icon: "icons/svg/aura.svg",
-// 				origin: owner.uuid,
-// 				"duration.rounds": li.dataset.effectType === "temporary" ? 1 : undefined,
-// 				disabled: li.dataset.effectType === "inactive"
-// 			}]);
-// 		case "edit":
-// 			return effect.sheet.render(true);
-// 		case "delete":
-// 			return effect.delete();
-// 		case "toggle":
-// 			return effect.update({disabled: !effect.data.disabled});
-// 	}
-// }
-
-// /**
-//  * Prepare the data structure for Active Effects which are currently applied to an Actor or Item.
-//  * @param {ActiveEffect[]} effects    The array of Active Effect instances to prepare sheet data for
-//  * @return {object}                   Data for rendering
-//  */
-// export function prepareActiveEffectCategories(effects) {
-
-// 		// Define effect header categories
-// 		const categories = {
-// 			temporary: {
-// 				type: "temporary",
-// 				label: game.i18n.localize("DND4EBETA.EffectTemporary"),
-// 				effects: []
-// 			},
-// 			passive: {
-// 				type: "passive",
-// 				label: game.i18n.localize("DND4EBETA.EffectPassive"),
-// 				effects: []
-// 			},
-// 			inactive: {
-// 				type: "inactive",
-// 				label: game.i18n.localize("DND4EBETA.EffectInactive"),
-// 				effects: []
-// 			}
-// 		};
-
-// 		// Iterate over active effects, classifying them into categories
-// 		for ( let e of effects ) {
-// 			e._getSourceName(); // Trigger a lookup for the source name
-// 			if ( e.data.disabled ) categories.inactive.effects.push(e);
-// 			else if ( e.isTemporary ) categories.temporary.effects.push(e);
-// 			else categories.passive.effects.push(e);
-// 		}
-// 		return categories;
-// }
