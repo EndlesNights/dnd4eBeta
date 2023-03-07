0b5fbf680ce8315797678afc7d02af8d0a7943b2
EndlesNights <endlesnights@gmail.com>
2023-03-07 18:46:49 -0500

    0.3.23

--- module/actor/actor-sheet.js
+++ module/actor/actor-sheet.js
@@ -51,7 +51,7 @@ export default class ActorSheet4e extends ActorSheet {
 		return mergeObject(super.defaultOptions, {
 			classes: ["dnd4eAltus", "sheet", "actor"],
 			width: 844,
-			height: 915,
+			height: 957,
 			tabs: [{
 				navSelector: ".sheet-tabs",
 				contentSelector: ".sheet-body",
