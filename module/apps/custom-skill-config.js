651bf3ddbb9978f90b7b958f6661320400e8b36d
EndlesNights <endlesnights@gmail.com>
2023-08-22 22:58:36 -0400

    custom skills

--- module/apps/custom-skill-config.js
+++ module/apps/custom-skill-config.js
@@ -1,3 +1,4 @@
+import { Helper } from "../helper.js"
 
 export default class CustomSkillConfig extends FormApplication {
     static get defaultOptions(){
@@ -16,6 +17,41 @@ export default class CustomSkillConfig extends FormApplication {
     }
 
     async getData(options) {
-        return {};
+        return game.settings.get("dnd4e", "custom-skills");;
     }
+
+    async _updateObject(event, formData) {
+        console.log(formData)
+    }
+
+    /** @override */
+	activateListeners(html) {
+		super.activateListeners(html);
+		if ( this.isEditable ) {
+			html.find('.bonus-add').click(this._onBonusAdd.bind(this));
+			html.find('.bonus-delete').click(this._onBonusDelete.bind(this));
+		}
+	}
+	
+	_onBonusAdd(event) {
+		console.log(this)
+		console.log(this.object)
+        event.preventDefault();
+		// const bonusData = Helper.byString(this.options.target, this.object).bonus;
+		const bonusData = [];
+		const newBonus =[{}];
+		this.position.height += 76;
+        
+        return 
+		// return this.object.update({[`${this.options.target}.bonus`]: bonusData.concat(newBonus)});
+	}
+	
+	_onBonusDelete(event) {
+		event.preventDefault();
+		const div = event.currentTarget.closest(".bonus-part");
+		const bonus = duplicate(Helper.byString(this.options.target, this.object).bonus);
+		bonus.splice(Number(div.dataset.bonusPart), 1);
+		this.position.height -= 76;
+		return this.object.update({[`${this.options.target}.bonus`]: bonus});
+	}
 }
