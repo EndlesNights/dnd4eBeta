0eedff2fe78f12876de508dd035cfab2ae8c5156
EndlesNights <endlesnights@gmail.com>
2023-09-16 18:06:10 -0400

    error checks

--- module/apps/turns.js
+++ module/apps/turns.js
@@ -15,16 +15,19 @@ export class Turns{
 		//t current turn
 		for(let t of game.combat.turns){
 			
+			if(!t.token?.actor?.effects){
+				continue;
+			}
+
 			let toDelete = [];
 			
-			for(let e of t.token?.actor?.effects){
+			for(let e of t.token.actor.effects){
 				const effectData = e.flags.dnd4e?.effectData;
 				const durationType = effectData?.durationType;
 	
 				if(!durationType){
 					continue;
 				}
-
 				else if(durationType === "endOfTargetTurn"){
 					if(currentInit <= effectData.durationTurnInit && currentRound >= e.duration.rounds && t.id === game.combat.combatant.id
 						|| (currentRound > e.duration.rounds && t.id === game.combat.combatant.id) ){
@@ -76,28 +79,33 @@ export class Turns{
 
 		// Signal the current actor to check end-of-turn saves
 		const currentActor = game.combat.turns[currentTurn].token?.actor
-		const targetUser = Helper.firstOwner(currentActor);
-		//console.log(targetUser);
-		//console.log(currentActor);
-		if(game.user.isGM && targetUser.isGM || game.user == targetUser){
-			await currentActor.promptEoTSavesSocket();
-		} else {
-			game.socket.emit('system.dnd4e', {
-				actorID: currentActor.id,
-				tokenID: game.combat.turns[currentTurn]?.token.id || null,
-				operation: 'promptEoTSaves',
-				user: targetUser.id,
-				scene: canvas.scene.id,
-				targetUser: targetUser.id
-			});
+
+		if(currentActor){
+			const targetUser = Helper.firstOwner(currentActor);
+
+			if(game.user.isGM && targetUser.isGM || game.user == targetUser){
+				await currentActor.promptEoTSavesSocket();
+			} else {
+				game.socket.emit('system.dnd4e', {
+					actorID: currentActor.id,
+					tokenID: game.combat.turns[currentTurn]?.token.id || null,
+					operation: 'promptEoTSaves',
+					user: targetUser.id,
+					scene: canvas.scene.id,
+					targetUser: targetUser.id
+				});
+			}
 		}
 
-		//Triggers for the beginning of the next turn
-		Helper.rechargeItems(game.combat.turns[nextTurn].token?.actor, ["round"]);
 		
 		// After EoT durations are resolved, collect ongoing damage instances from effects
 		const nextCombatant = game.combat.turns[nextTurn].token?.actor || null;
 		
+		if(nextCombatant){
+			//Triggers for the beginning of the next turn
+			Helper.rechargeItems(nextCombatant, ["round"]);
+		}
+
 		if(nextCombatant){
 			const nextTargetUser = Helper.firstOwner(nextCombatant);
 			if(game.user.isGM && nextTargetUser.isGM){
