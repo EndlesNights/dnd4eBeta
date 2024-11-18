3a2a5b71e1ca496ad1535b8e46878880591e3d0d
Fox "The Art Gremblin" Lee <fox@munchlax.org>
2024-11-18 15:46:08 +1100

    Updated tiebreaker behaviour for revised setting

--- module/combat.js
+++ module/combat.js
@@ -2,7 +2,7 @@
 /**
  * Override the default Initiative formula to customize special behaviors of the D&D4e system.
  * Apply advantage, proficiency, or bonuses where appropriate
- * Apply the dexterity score as a decimal tiebreaker if requested
+ * Apply a tiebreaker according to system preference
  * See Combat._getInitiativeFormula for more detail.
  */
 export const _getInitiativeFormula = function() {
@@ -13,10 +13,10 @@ export const _getInitiativeFormula = function() {
 	const parts = ["1d20", init,];
 	if ( actor.getFlag("dnd4e", "initiativeAdv") ) parts[0] = "2d20kh";
 	
-	if (tiebreaker === false) {
+	if (tiebreaker === 'system') {
 		//Official system behaviour: append ititiative modifier as tiebreaker
 		parts.push(actor.system.attributes.init.value / 100);
-	} else {
+	} else if (tiebreaker === 'dex') {
 		//Optional override: append raw dexterity score as tiebreaker
 		parts.push(actor.system.abilities.dex.value / 100);
 	}
