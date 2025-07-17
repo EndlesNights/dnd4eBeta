c62d75751a8cbde1319bd8533f512c5b6cdf2c43
SagaTympana <96776279+SagaTympana@users.noreply.github.com>
2025-07-17 19:17:17 -0400

    Update actor-sheet.js

--- module/actor/actor-sheet.js
+++ module/actor/actor-sheet.js
@@ -195,11 +195,11 @@ export default class ActorSheet4e extends ActorSheet {
 				const res = actorData.resources[r] || {};
 				res.name = r;
 				res.placeholder = game.i18n.localize("DND4E.Resource"+r.titleCase());
-				if (res && res.max === 0) {
+				if (res && res.max <= 0) {
 		                    delete res.max;
 		                    delete res.value;
 		                } else if (res && res.value < 0) {
-		                    delete res.value;
+		                    res.value = 0;
 		                }
 				obj[r] = res
 				return obj;
