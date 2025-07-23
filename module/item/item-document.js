a2676145b6d4cc544cdf58441d47845d852c6403
Fox "The Art Gremblin" Lee <fox@munchlax.org>
2025-07-23 17:34:59 +1000

    Embed data for all chat cards
    This was previously just for consumables that had been deleted when rolling, but adding it for all chat card means we can make temporary/self-deleting powers work too.

--- module/item/item-document.js
+++ module/item/item-document.js
@@ -925,9 +925,7 @@ export default class Item4e extends Item {
 		};
 		
 		// If the Item was destroyed in the process of displaying its card - embed the item data in the chat message
-		if ( (this.type === "consumable") && !this.actor.items.has(this.id) ) {
-			chatData.flags["dnd4e.itemData"] = templateData.item;
-		}
+		chatData.flags["dnd4e.itemData"] = templateData.item;
 		
 		// Embed variance in the chat message, so buttons can be aware of it
 		if (variance) {
