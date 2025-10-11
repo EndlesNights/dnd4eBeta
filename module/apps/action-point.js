3216c4bd260b42b9811122733bd9af2545f44817
SagaTympana <96776279+SagaTympana@users.noreply.github.com>
2025-10-10 21:07:53 -0400

    AppV2 Action Point

--- module/apps/action-point.js
+++ module/apps/action-point.js
@@ -2,30 +2,37 @@ import DocumentSheet4e from "./DocumentSheet4e.js"
 
 export class ActionPointDialog extends DocumentSheet4e {
 
-	static get defaultOptions() {
-		const options = super.defaultOptions;
-		return foundry.utils.mergeObject(options, {
-			id: "action-point",
-			classes: ["action-point"],
-			template: "systems/dnd4e/templates/apps/action-point.html",
-			width: 500,
-			closeOnSubmit: true
-		});
+	static DEFAULT_OPTIONS = {
+		id: "action-point",
+        form: {
+            closeOnSubmit: true
+        },
+        position: {
+            width: 500,
+            height: "auto"
+        }
 	}
 	
 	get title() {
-		return `${this.object.name} - Action Point`;
+		return `${this.document.name} - Action Point`;
 	}
 
+    static PARTS = {
+        actionPoint: {
+            template: "systems/dnd4e/templates/apps/action-point.hbs"
+        }
+    }
+
 	/** @override */
-	getData() {
-		const extra = this.object.system.actionpoints.custom !== "" ? this.object.system.actionpoints.custom.split("\n") : "";
-		return { system: this.object.system, extra: extra };
+	_prepareContext() {
+		const extra = this.document.system.actionpoints.custom !== "" ? this.document.system.actionpoints.custom.split("\n") : "";
+		return { system: this.document.system, extra: extra };
 	}
+
 	async _updateObject(event, formData) {
 		
 		const options = this.options;
 
-		this.object.actionPoint(event, options);
+		this.document.actionPoint(event, options);
 	}
 }
