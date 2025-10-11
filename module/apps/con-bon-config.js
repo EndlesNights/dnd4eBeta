527f476e8d9ff630550c1ed7de09300e2ef5938c
SagaTympana <96776279+SagaTympana@users.noreply.github.com>
2025-10-11 00:10:35 -0400

    AppV2 Conditional Bonus Config

--- module/apps/con-bon-config.js
+++ module/apps/con-bon-config.js
@@ -2,42 +2,51 @@ import { AttributeBonusDialog } from "./attribute-bonuses.js";
 import DocumentSheet4e from "./DocumentSheet4e.js"
 
 export default class ConBonConfig extends DocumentSheet4e {
-	static get defaultOptions() {
-		const options = super.defaultOptions;
-		return foundry.utils.mergeObject(options, {
-			id: "con-bon-config",
-			classes: ["dnd4e", "con-bon-config"],
-			template: "systems/dnd4e/templates/apps/con-bon-config.html",
-			width: 320,
-			closeOnSubmit: false
-		});
-	}
+	static DEFAULT_OPTIONS = {
+        id: "con-bon-config",
+        classes: ["dnd4e", "con-bon-config"],
+        form: {
+            closeOnSubmit: false
+        },
+        position: {
+            width: 320,
+            height: "auto"
+        }
+    }
+
 	get title() {
-		return `${this.object.name}—${game.i18n.localize( 'DND4E.CommonAttackBonuses')}`;
+		return `${this.document.name}—${game.i18n.localize( 'DND4E.CommonAttackBonuses')}`;
 	}
 
+    static PARTS = {
+        ConBonConfig: {
+            template: "systems/dnd4e/templates/apps/con-bon-config.hbs"
+        }
+    }
+
 	/** @override */
-	getData() {
-		return {system: this.object.system}
+	_prepareContext() {
+		return {system: this.document.system}
 	}
+
 	async _updateObject(event, formData) {
 		const updateData = {};
 		for(let system in formData) { updateData[`${system}`] = formData[`${system}`];}
-		this.object.update(updateData);
+		this.document.update(updateData);
 	}
 
-	activateListeners(html) {
-		super.activateListeners(html);
-		if (!this.options.editable) return;
-		html.find('.condition-mod-bonus').click(this._onConditionBonus.bind(this));
+	_onRender(context, options) {
+		if (!this.document.testUserPermission(game.user, this.options.editPermission)) return;
+		this.element.querySelectorAll('.condition-mod-bonus').forEach((el) => {
+            el.addEventListener("click", (this._onConditionBonus.bind(this)));
+        });
 	}
 
 	_onConditionBonus(event) {
 		event.preventDefault();
 		const conditionId = event.currentTarget.parentElement.dataset.mod;
 		const target = `system.commonAttackBonuses.${conditionId}`;
-		const options = {target: target, label: `${game.i18n.format('DND4E.CommonAttackBonusesConfig', {condition:game.i18n.localize(this.object.system.commonAttackBonuses[conditionId].label)})}` };
-		new AttributeBonusDialog(this.object, options).render(true);
+		const options = { document: this.document, target: target, label: `${game.i18n.format('DND4E.CommonAttackBonusesConfig', {condition:game.i18n.localize(this.document.system.commonAttackBonuses[conditionId].label)})}` };
+		new AttributeBonusDialog(options).render(true);
 	}
-	
 }
