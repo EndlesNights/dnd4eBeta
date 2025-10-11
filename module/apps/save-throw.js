58096595a64a461354bbe9fe4e7f14b5dc6278cb
SagaTympana <96776279+SagaTympana@users.noreply.github.com>
2025-10-11 16:28:04 -0400

    AppV2 Saving Throw

--- module/apps/save-throw.js
+++ module/apps/save-throw.js
@@ -2,25 +2,35 @@ import DocumentSheet4e from "./DocumentSheet4e.js"
 
 export class SaveThrowDialog extends DocumentSheet4e {
 
-	static get defaultOptions() {
-		const options = super.defaultOptions;
-		return foundry.utils.mergeObject(options, {
-			id: "save-throw",
-			classes: ["dnd4e", "actor-save-throw"],
-			template: "systems/dnd4e/templates/apps/save-throw.html",
+	static DEFAULT_OPTIONS = {
+		id: "save-throw",
+		classes: ["dnd4e", "actor-save-throw"],
+		form: {
+			closeOnSubmit: true,
+			handler: SaveThrowDialog.#onSubmit
+		},
+		position: {
 			width: 500,
-			closeOnSubmit: true
-		});
+			height: "auto",
+		},
+		tag: "form"
 	}
+
 	get title() {
-		return `${this.object.name} - ${game.i18n.format("DND4E.SavingThrow")}`;
+		return `${this.document.name} - ${game.i18n.format("DND4E.SavingThrow")}`;
+	}
+
+	static PARTS = {
+		SaveThrowDialog: {
+			template: "systems/dnd4e/templates/apps/save-throw.hbs"
+		}
 	}
 
 	/** @override */
-	getData() {
+	_prepareContext() {
 		const options = this.options;
 		let savableEffects = [];
-		const actor = this.object;
+		const actor = this.document;
 		if (actor && !options.effectSave) {
 			Array.from(actor.effects).forEach((e) => {
 				if (e.flags.dnd4e?.effectData?.durationType === 'saveEnd') savableEffects.push({name: e.name, id: e.id});
@@ -42,21 +52,22 @@ export class SaveThrowDialog extends DocumentSheet4e {
 	async _onChangeInput(event) {
 		const target = event.target;
 		if (target?.name !== "saveAgainst") return;
-		this.options.saveDC = this.object.effects.get(target.value)?.flags.dnd4e?.effectData?.saveDC;
+		this.options.saveDC = this.document.effects.get(target.value)?.flags.dnd4e?.effectData?.saveDC;
 		this.options.saveAgainst = target.value;
 		this.render();
 	}
 
-	async _updateObject(event, formData) {
-		const options = this.options;
-		options.dc = formData.dc;
-		options.save = formData.save;
-		options.rollMode = formData.rollMode;
-		if (formData.saveAgainst) {
-			options.effectSave = true;
-			options.effectId = formData.saveAgainst
+	static #onSubmit(event, form, formData) {
+		const saveData = foundry.utils.expandObject(formData.object);
+		if (saveData.saveAgainst) {
+			saveData.effectSave = true;
+			saveData.effectId = formData.saveAgainst
 		}
 
-		this.document.rollSave(event, options);
+		this.document.rollSave(event, {
+			...this.options,
+			...saveData
+		});
 	}
+
 }
