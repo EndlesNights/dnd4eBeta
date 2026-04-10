47dc0864e5620087dc092b215cf987727f84c7fe
draconas1 <draconas@gmail.com>
2026-04-10 21:12:17 +0100

    Remove console logs from hooks.js
    Remove the console.log statements from hooks.js
    
    Eurgh, how did I miss these?  I was sure I had removed them after testing.

--- module/hooks.js
+++ module/hooks.js
@@ -64,7 +64,6 @@ TokenBarHooks.powersBySheetGroup = (actor) => {
 TokenBarHooks.generateItemTooltip = async (actor, item) => {
     //see actor-sheet.js line 383
     const chatdata = await item.getChatData({secrets: actor.isOwner})
-    console.log(chatdata)
     if (item.type === "power" && item.system.autoGenChatPowerCard) {
         let attackBonus = null;
         if(item.hasAttack){
@@ -75,7 +74,6 @@ TokenBarHooks.generateItemTooltip = async (actor, item) => {
             async: true,
             relativeTo: actor
         });
-        console.log(enrichedDetailsText)
         return enrichedDetailsText
     }
     else {
