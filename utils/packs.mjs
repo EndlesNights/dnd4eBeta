cf1405109d58a6c4a7de9e8f5a78368259c82853
SagaTympana <96776279+SagaTympana@users.noreply.github.com>
2026-05-21 10:25:54 -0400

    Update packs.mjs

--- utils/packs.mjs
+++ utils/packs.mjs
@@ -11,41 +11,42 @@ const PACK_DEST = "packs";
 
 const PACK_SRC = "packs/_source";
 
+// eslint-disable-next-line no-undef
 const argv = yargs(hideBin(process.argv))
-  .command(packageCommand())
-  .help().alias("help", "h")
-  .argv;
+	.command(packageCommand())
+	.help().alias("help", "h")
+	.argv;
 
 function packageCommand() {
-  return {
-    command: "package [action] [pack] [entry]",
-    describe: "Manage packages",
-    builder: yargs => {
-      yargs.positional("action", {
-        describe: "The action to perform.",
-        type: "string",
-        choices: ["unpack", "pack", "clean"]
-      });
-      yargs.positional("pack", {
-        describe: "Name of the pack upon which to work.",
-        type: "string"
-      });
-      yargs.positional("entry", {
-        describe: "Name of any entry within a pack upon which to work. Only applicable to extract & clean commands."
-      })
-    },
-    handler: async argv => {
-      const { action, pack, entry } = argv;
-      switch (action) {
-        case "clean":
-          return await cleanPacks(pack, entry);
-        case "pack":
-          return await compilePacks(pack);
-        case "unpack":
-          return await extractPacks(pack, entry);
-      }
-    }
-  };
+	return {
+		command: "package [action] [pack] [entry]",
+		describe: "Manage packages",
+		builder: yargs => {
+			yargs.positional("action", {
+				describe: "The action to perform.",
+				type: "string",
+				choices: ["unpack", "pack", "clean"],
+			});
+			yargs.positional("pack", {
+				describe: "Name of the pack upon which to work.",
+				type: "string",
+			});
+			yargs.positional("entry", {
+				describe: "Name of any entry within a pack upon which to work. Only applicable to extract & clean commands.",
+			});
+		},
+		handler: async argv => {
+			const { action, pack, entry } = argv;
+			switch (action) {
+				case "clean":
+					return await cleanPacks(pack, entry);
+				case "pack":
+					return await compilePacks(pack);
+				case "unpack":
+					return await extractPacks(pack, entry);
+			}
+		},
+	};
 }
 
 /* ----------------------------------------- */
@@ -59,28 +60,28 @@ function packageCommand() {
  * @param {boolean} [options.clearSourceId=true]  Should the core sourceId flag be deleted.
  * @param {number} [options.ownership=0]          Value to reset default ownership to.
  */
-function cleanPackEntry(data, {clearSourceId=true, ownership=0}={}) {
-  if (data.ownership) data.ownership = {default: ownership};
-  if (!data.flags) data.flags = {};
-  if (clearSourceId) {
-    delete data._stats?.compendiumSource;
-    delete data.flags.core?.sourceId;
-  }
-  delete data.flags.importSource;
-  delete data.flags.exportSource;
-  if (data._stats?.lastModifiedBy) data._stats.lastModifiedBy = "dnd4ebuilder0000";
+function cleanPackEntry(data, { clearSourceId = true, ownership = 0 } = {}) {
+	if (data.ownership) data.ownership = { default: ownership };
+	if (!data.flags) data.flags = {};
+	if (clearSourceId) {
+		delete data._stats?.compendiumSource;
+		delete data.flags.core?.sourceId;
+	}
+	delete data.flags.importSource;
+	delete data.flags.exportSource;
+	if (data._stats?.lastModifiedBy) data._stats.lastModifiedBy = "dnd4ebuilder0000";
 
-  // Remove empty entries in flags
-  Object.entries(data.flags).forEach(([key, contents]) => {
-    if (Object.keys(contents).length === 0) delete data.flags[key];
-  });
+	// Remove empty entries in flags
+	Object.entries(data.flags).forEach(([key, contents]) => {
+		if (Object.keys(contents).length === 0) delete data.flags[key];
+	});
 
-  if (data.effects) data.effects.forEach(i => cleanPackEntry(i, { clearSourceId: false }));
-  if (data.items) data.items.forEach(i => cleanPackEntry(i, { clearSourceId: false }));
-  if (data.pages) data.pages.forEach(i => cleanPackEntry(i, { ownership: -1 }));
-  if (data.system?.description?.value ) data.system.description.value = cleanString(data.system.description.value);
-  if (data.label) data.label = cleanString(data.label);
-  if (data.name) data.name = cleanString(data.name);
+	if (data.effects) data.effects.forEach(i => cleanPackEntry(i, { clearSourceId: false }));
+	if (data.items) data.items.forEach(i => cleanPackEntry(i, { clearSourceId: false }));
+	if (data.pages) data.pages.forEach(i => cleanPackEntry(i, { ownership: -1 }));
+	if (data.system?.description?.value) data.system.description.value = cleanString(data.system.description.value);
+	if (data.label) data.label = cleanString(data.label);
+	if (data.name) data.name = cleanString(data.name);
 }
 
 /**
@@ -89,7 +90,7 @@ function cleanPackEntry(data, {clearSourceId=true, ownership=0}={}) {
  * @returns {string}    The cleaned string.
  */
 function cleanString(str) {
-  return str.replace(/\u2060/gu, "").replace(/[‘’]/gu, "'").replace(/[“”]/gu, '"');
+	return str.replace(/\u2060/gu, "").replace(/[‘’]/gu, "'").replace(/[“”]/gu, "\"");
 }
 
 /**
@@ -102,38 +103,38 @@ function cleanString(str) {
  * - `npm run build:clean -- featuresPack "Barbarian Class"` - Only clean a single item from the specified compendium.
  */
 async function cleanPacks(packName, entryName) {
-  entryName = entryName?.toLowerCase();
-  const folders = fs.readdirSync(PACK_SRC, { withFileTypes: true }).filter(file =>
-    file.isDirectory() && ( !packName || (packName === file.name) )
-  );
+	entryName = entryName?.toLowerCase();
+	const folders = fs.readdirSync(PACK_SRC, { withFileTypes: true }).filter(file =>
+		file.isDirectory() && (!packName || (packName === file.name)),
+	);
 
-  /**
+	/**
    * Walk through directories to find files.
    * @param {string} directoryPath
    * @yields {string}
    */
-  async function* _walkDir(directoryPath) {
-    const directory = await readdir(directoryPath, { withFileTypes: true });
-    for ( const entry of directory ) {
-      const entryPath = path.join(directoryPath, entry.name);
-      if ( entry.isDirectory() ) yield* _walkDir(entryPath);
-      else if ( path.extname(entry.name) === ".json" ) yield entryPath;
-    }
-  }
+	async function* _walkDir(directoryPath) {
+		const directory = await readdir(directoryPath, { withFileTypes: true });
+		for (const entry of directory) {
+			const entryPath = path.join(directoryPath, entry.name);
+			if (entry.isDirectory()) yield* _walkDir(entryPath);
+			else if (path.extname(entry.name) === ".json") yield entryPath;
+		}
+	}
 
-  for ( const folder of folders ) {
-    for await ( const src of _walkDir(path.join(PACK_SRC, folder.name)) ) {
-      const json = JSON.parse(await readFile(src, { encoding: "utf8" }));
-      if ( entryName && (entryName !== json.name.toLowerCase()) ) continue;
-      if ( !json._id || !json._key ) {
-        console.log(`Failed to clean \x1b[31m${src}\x1b[0m, must have _id and _key.`);
-        continue;
-      }
-      cleanPackEntry(json);
-      fs.rmSync(src, { force: true });
-      writeFile(src, `${JSON.stringify(json, null, 2)}\n`, { mode: 0o664 });
-    }
-  }
+	for (const folder of folders) {
+		for await (const src of _walkDir(path.join(PACK_SRC, folder.name))) {
+			const json = JSON.parse(await readFile(src, { encoding: "utf8" }));
+			if (entryName && (entryName !== json.name.toLowerCase())) continue;
+			if (!json._id || !json._key) {
+				console.log(`Failed to clean \x1b[31m${src}\x1b[0m, must have _id and _key.`);
+				continue;
+			}
+			cleanPackEntry(json);
+			fs.rmSync(src, { force: true });
+			writeFile(src, `${JSON.stringify(json, null, 2)}\n`, { mode: 0o664 });
+		}
+	}
 }
 
 /* ----------------------------------------- */
@@ -148,16 +149,16 @@ async function cleanPacks(packName, entryName) {
  * - `npm run build:db -- featuresPack` - Only compile the specified pack.
  */
 async function compilePacks(packName) {
-  // Determine which source folders to process
-  const folders = fs.readdirSync(PACK_SRC, { withFileTypes: true }).filter(file =>
-    file.isDirectory() && ( !packName || (packName === file.name) )
-  );
+	// Determine which source folders to process
+	const folders = fs.readdirSync(PACK_SRC, { withFileTypes: true }).filter(file =>
+		file.isDirectory() && (!packName || (packName === file.name)),
+	);
 
-  for ( const folder of folders ) {
-    const src = path.join(PACK_SRC, folder.name);
-    const dest = path.join(PACK_DEST, folder.name);
-    await compilePack(src, dest, { recursive: true, log: true, transformEntry: cleanPackEntry });
-  }
+	for (const folder of folders) {
+		const src = path.join(PACK_SRC, folder.name);
+		const dest = path.join(PACK_DEST, folder.name);
+		await compilePack(src, dest, { recursive: true, log: true, transformEntry: cleanPackEntry });
+	}
 }
 
 /* ----------------------------------------- */
@@ -174,56 +175,56 @@ async function compilePacks(packName) {
  * - `npm build:source -- featuresPack "Barbarian Class"` - Only extract a single item from the specified compendium.
  */
 async function extractPacks(packName, entryName) {
-  entryName = entryName?.toLowerCase();
+	entryName = entryName?.toLowerCase();
 
-  // Load system.json.
-  const system = JSON.parse(fs.readFileSync("./system.json", { encoding: "utf8" }));
+	// Load system.json.
+	const system = JSON.parse(fs.readFileSync("./system.json", { encoding: "utf8" }));
 
-  // Determine which source packs to process.
-  const packs = system.packs.filter(p => !packName || p.name === packName);
+	// Determine which source packs to process.
+	const packs = system.packs.filter(p => !packName || (p.name === packName));
 
-  for ( const packInfo of packs ) {
-    const dest = path.join(PACK_SRC, packInfo.name);
+	for (const packInfo of packs) {
+		const dest = path.join(PACK_SRC, packInfo.name);
 
-    const folders = {};
-    const containers = {};
-    await extractPack(packInfo.path, dest, {
-      log: false, transformEntry: e => {
-        if ( e._key.startsWith("!folders") ) folders[e._id] = { name: slugify(e.name), folder: e.folder };
-        else if ( e.type === "container" ) containers[e._id] = {
-          name: slugify(e.name), container: e.system?.container, folder: e.folder
-        };
-        return false;
-      }
-    });
-    const buildPath = (collection, entry, parentKey) => {
-      let parent = collection[entry[parentKey]];
-      entry.path = entry.name;
-      while ( parent ) {
-        entry.path = path.join(parent.name, entry.path);
-        parent = collection[parent[parentKey]];
-      }
-    };
-    Object.values(folders).forEach(f => buildPath(folders, f, "folder"));
-    Object.values(containers).forEach(c => {
-      buildPath(containers, c, "container");
-      const folder = folders[c.folder];
-      if ( folder ) c.path = path.join(folder.path, c.path);
-    });
+		const folders = {};
+		const containers = {};
+		await extractPack(packInfo.path, dest, {
+			log: false, transformEntry: e => {
+				if (e._key.startsWith("!folders")) folders[e._id] = { name: slugify(e.name), folder: e.folder };
+				else if (e.type === "container") containers[e._id] = {
+					name: slugify(e.name), container: e.system?.container, folder: e.folder,
+				};
+				return false;
+			},
+		});
+		const buildPath = (collection, entry, parentKey) => {
+			let parent = collection[entry[parentKey]];
+			entry.path = entry.name;
+			while (parent) {
+				entry.path = path.join(parent.name, entry.path);
+				parent = collection[parent[parentKey]];
+			}
+		};
+		Object.values(folders).forEach(f => buildPath(folders, f, "folder"));
+		Object.values(containers).forEach(c => {
+			buildPath(containers, c, "container");
+			const folder = folders[c.folder];
+			if (folder) c.path = path.join(folder.path, c.path);
+		});
 
-    await extractPack(packInfo.path, dest, {
-      log: true, transformEntry: entry => {
-        if ( entryName && (entryName !== entry.name.toLowerCase()) ) return false;
-        cleanPackEntry(entry);
-      }, transformName: entry => {
-        if ( entry._id in folders ) return path.join(folders[entry._id].path, "_folder.json");
-        if ( entry._id in containers ) return path.join(containers[entry._id].path, "_container.json");
-        const outputName = slugify(entry.name);
-        const parent = containers[entry.system?.container] ?? folders[entry.folder];
-        return path.join(parent?.path ?? "", `${outputName}.json`);
-      }
-    });
-  }
+		await extractPack(packInfo.path, dest, {
+			log: true, transformEntry: entry => {
+				if (entryName && (entryName !== entry.name.toLowerCase())) return false;
+				cleanPackEntry(entry);
+			}, transformName: entry => {
+				if (entry._id in folders) return path.join(folders[entry._id].path, "_folder.json");
+				if (entry._id in containers) return path.join(containers[entry._id].path, "_container.json");
+				const outputName = slugify(entry.name);
+				const parent = containers[entry.system?.container] ?? folders[entry.folder];
+				return path.join(parent?.path ?? "", `${outputName}.json`);
+			},
+		});
+	}
 }
 
 /**
@@ -232,5 +233,5 @@ async function extractPacks(packName, entryName) {
  * @returns {string}
  */
 function slugify(name) {
-  return name.toLowerCase().replace("'", "").replace(/[^a-z0-9]+/gi, " ").trim().replace(/\s+|-{2,}/g, "-");
+	return name.toLowerCase().replace("'", "").replace(/[^a-z0-9]+/gi, " ").trim().replace(/\s+|-{2,}/g, "-");
 }
