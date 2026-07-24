4bf5c0f5ae2864206c8a2ec4fd4a64f77db51bcf
Fox "The Art Gremblin" Lee <fox@munchlax.org>
2026-07-24 15:00:50 +1000

    Added to legacy statuses

--- module/applications/apps/custom-status-config.mjs
+++ module/applications/apps/custom-status-config.mjs
@@ -124,6 +124,16 @@ export default class CustomStatusConfig extends foundry.applications.api.Handleb
 			img: "systems/dnd4e/icons/statusEffects/disarmed.svg",
 			description: "EFFECTDESC.disarmed",
 		},
+    flying: {
+      name: "EFFECT.statusFlying",
+      img: "systems/dnd4e/icons/statusEffects/flying.svg",
+      description: "EFFECTDESC.flying",
+    },
+    mounted: {
+      name: "EFFECT.statusMounted",
+      img: "systems/dnd4e/icons/statusEffects/mounted.svg",
+      description: "EFFECTDESC.mounted",
+    },
 	};
 
 	constructor(...args) {
