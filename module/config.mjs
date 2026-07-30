d9179d5620797a015c20d7239c92376dd9d1afe5
SagaTympana <96776279+SagaTympana@users.noreply.github.com>
2026-07-30 16:50:41 -0400

    Move Obscurement Labels
    These are only used by the region behavior config.

--- module/config.mjs
+++ module/config.mjs
@@ -2265,14 +2265,6 @@ DND4E.OBSCUREMENT = {
 	TOTAL: 3,
 };
 
-DND4E.OBSCUREMENT_LABELS = {
-	[DND4E.OBSCUREMENT.NONE]: "DND4E.None",
-	[DND4E.OBSCUREMENT.LIGHT]: "EFFECT.statusObscuredLight",
-	[DND4E.OBSCUREMENT.HEAVY]: "EFFECT.statusObscuredHeavy",
-	[DND4E.OBSCUREMENT.TOTAL]: "EFFECT.statusObscuredTotal",
-};
-preLocalize("OBSCUREMENT_LABELS");
-
 // Configure allowed status flags
 DND4E.allowedActorFlags = [
 	"isPolymorphed", "originalActor",
