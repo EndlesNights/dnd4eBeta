bfcbbc32e8eb2270b7ec3df69fa85aa5ac37cc88
Fox "The Art Gremblin" Lee <fox@munchlax.org>
2026-08-01 18:57:39 +1000

    Harmonise with previous merge

--- module/utils/utils.mjs
+++ module/utils/utils.mjs
@@ -1052,7 +1052,7 @@ export async function applyEffectsToTokens(effectMap, tokenTarget, condition, pa
 					newEffectData.system.saveDC = String(Number(newEffectData.system.saveDC) + dcBonus);
 				}
 
-				if (e.statuses.length && game.settings.get("dnd4e", "markAutomation")) {
+				if (e.statuses.length && game.settings.get("dnd4e", "dynamicAutomation")) {
 					const hasMark = e.statuses.some(s => s === "mark");
 						
 					if (hasMark) {
