d0ee4c683eba218370a9d380c1753fdb9eb06d8b
Fox "The Art Gremblin" Lee <fox@munchlax.org>
2026-08-14 16:21:13 +1000

    Powers infer auraSize if none given

--- module/documents/item.mjs
+++ module/documents/item.mjs
@@ -704,7 +704,10 @@ export default class Item4e extends Item {
 
 		// Powers
 		else if (itemData.type === "power") {
-			try {				
+			try {
+				//Needed for powers to create aura regions, since they don't have this field
+				if(system.effectType.aura && !system?.auraSize) system.auraSize = system?.area || 0;
+
 				//Summary Line
 				let summaryText = "";
 				
